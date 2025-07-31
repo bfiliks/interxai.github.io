209aa7e6def19d5633b8e0cec7948ded81fd5e2d
Felix Oke <bfiliks4xt@gmail.com>
2025-07-31 05:23:02 -0500

    Delete app/intertextual_linker.py

--- app/intertextual_linker.py
+++
@@ -1,8 +0,0 @@
-"""
-intertextual_linker.py
-Links to external texts using APIs (e.g., Wikidata, JSTOR Labs).
-"""
-
-def get_intertextual_links(text):
-    # TODO: Call external APIs and return links
-    return ["Beloved by Toni Morrison", "Psalm 137", "Exodus"]
